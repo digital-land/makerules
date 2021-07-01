8b52c70221c177b358fd255e84ae0c49565c5471
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2021-07-01 10:05:31 +0100

    Revert previous commit

--- render.mk
+++ render.mk
@@ -36,7 +36,6 @@ endif
 TEMPLATE_FILES=$(wildcard templates/*)
 
 $(VIEW_MODEL):
-	mkdir dataset/
 ifeq ($(RENDER_FLAGS),"--cross-reference")
 	curl -qfsL 'http://datasette-demo.digital-land.info/view_model.db' > $@
 else
