6be02890b71d3a420e016747a78914481c942d74
Feba <feba.rajan@levellingup.gov.uk>
2024-11-21 09:04:46 +0000

    updated CONFIG_URL(development)

--- pipeline.mk
+++ pipeline.mk
@@ -235,7 +235,7 @@ datasette:	metadata.json
 $(PIPELINE_DIR)%.csv:
 	@mkdir -p $(PIPELINE_DIR)
 	@if [ ! -f $@ ]; then \
-		echo "Config file $@ not found locally. Attempting to download..."; \
+		echo "Config file $@ not found locally. Attempting to download from s3...."; \
 		curl -qfsL '$(PIPELINE_CONFIG_URL)$(notdir $@)' -o $@ || \
 		(echo "File not found in config repo: $(notdir $@)" && exit 1); \
 	fi
