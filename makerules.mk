7b92e7e47ba750ac15e923f52c1eead0afc0d59d
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-02-04 15:53:25 +0000

    remove branch redirect

--- makerules.mk
+++ makerules.mk
@@ -12,7 +12,7 @@ SOURCE_URL=https://raw.githubusercontent.com/digital-land/
 endif
 
 ifeq ($(MAKERULES_URL),)
-MAKERULES_URL=$(SOURCE_URL)makerules/commands-refill-todays-log/
+MAKERULES_URL=$(SOURCE_URL)makerules/main/
 endif
 
 ifeq ($(DATASTORE_URL),)
