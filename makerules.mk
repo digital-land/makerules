6ce52fcc2be5d3ccb6020ab37151a8f953c3550d
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-02-05 10:27:11 +0000

    adds --size-only when incremental loading is off

--- makerules.mk
+++ makerules.mk
@@ -12,7 +12,7 @@ SOURCE_URL=https://raw.githubusercontent.com/digital-land/
 endif
 
 ifeq ($(MAKERULES_URL),)
-MAKERULES_URL=$(SOURCE_URL)makerules/main/
+MAKERULES_URL=$(SOURCE_URL)makerules/determine-new-resources/
 endif
 
 ifeq ($(DATASTORE_URL),)
