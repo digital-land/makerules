efa4af4090d0a09ca11b9f96fcb82c2e4cd2805c
CarlosCoelhoSL <110818364+CarlosCoelhoSL@users.noreply.github.com>
2025-02-10 16:01:51 +0000

    Fix log and resource download for no file found (#82)
    * adds checks that log.csv and resource.csv exist
    
    * adds echo URL
    
    * adds branch redirect
    
    * adds backslash
    
    * change status code check logic

--- collection.mk
+++ collection.mk
@@ -72,8 +72,15 @@ else ifeq ($(REGENERATE_LOG_OVERRIDE),True)
 	echo 'Syncing log files to local';
 	aws s3 sync s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/$(LOG_DIR) $(LOG_DIR) --only-show-errors;
 else
-	aws s3 cp s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/log.csv collection/log.csv --only-show-errors;
-	aws s3 cp s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/resource.csv collection/resource.csv --only-show-errors;
+	$(eval LOG_STATUS_CODE := $(shell curl -I -o /dev/null -s -w "%{http_code}" 's3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/log.csv'))
+	$(eval RESOURCE_STATUS_CODE := $(shell curl -I -o /dev/null -s -w "%{http_code}" 's3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/resource.csv'))
+	@if [ $(LOG_STATUS_CODE) -eq 200 ] && [ $(RESOURCE_STATUS_CODE) -eq 200 ]; then \
+		echo 'Downloading log.csv and resource.csv from s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/'; \
+		aws s3 cp s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/log.csv collection/log.csv --only-show-errors; \
+		aws s3 cp s3://$(COLLECTION_DATASET_BUCKET_NAME)/$(REPOSITORY)/collection/resource.csv collection/resource.csv --only-show-errors; \
+	else \
+		echo 'Unable to locate log.csv and resource.csv' ;\
+	fi
 endif
 
 first-pass:: collect
